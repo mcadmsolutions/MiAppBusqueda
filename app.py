ddadad6647f68e543f5eccca8eb4da064918940c
mcadmsolutions <mcadmsolutions@gmail.com>
2025-03-09 19:52:48 -0600

    Primer commit - Proyecto MiAppBusqueda

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import gspread
+from google.oauth2.service_account import Credentials
+
+app = Flask(__name__)
+
+# Credenciales de Google Sheets
+cred_path = "miappbusqueda.json"  # Asegúrate de que el archivo esté en la misma carpeta
+scopes = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = Credentials.from_service_account_file(cred_path, scopes=scopes)
+client = gspread.authorize(creds)
+
+# Abre la hoja de cálculo con el nombre correcto
+spreadsheet = client.open("Mapa Picking Json")
+sheet = spreadsheet.sheet1  # Primera hoja
+
+# Página principal (busqueda)
+@app.route('/', methods=['GET'])
+def index():
+    query = request.args.get('query', '').lower()  # Obtener la búsqueda y convertirla a minúsculas
+    data = []
+    message = None  # Mensaje por defecto vacío
+
+    if query:
+        # Filtrar los datos de acuerdo a la búsqueda
+        all_data = sheet.get_all_records()  # Obtener todos los registros de la hoja
+        for row in all_data:
+            # Convertimos los valores a cadena antes de hacer la comparación
+            modelo = str(row['MODELO']).lower()
+            nombre_corto = str(row['NOMBRE CORTO']).lower()
+
+            # Verificamos si la consulta aparece en el modelo o el nombre corto
+            if query in modelo or query in nombre_corto:
+                data.append(row)
+
+        if not data:
+            message = f"No se encontraron resultados para '{query}'."  # Mostrar el mensaje solo si no hay resultados
+
+    return render_template('index.html', data=data, query=query, message=message)
+
+# Página de la tabla
+@app.route('/tabla')
+def tabla():
+    # Leer todos los datos de la hoja, excepto los encabezados
+    data = sheet.get_all_records()  # Esto obtiene todos los datos de la hoja sin los encabezados
+
+    # Verifica si estamos obteniendo datos
+    print("Datos obtenidos desde Google Sheets:")
+    print(data)  # Esto imprimirá los datos en la consola
+
+    return render_template('tabla.html', data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
